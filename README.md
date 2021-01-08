1e1b335acef4719cf475e45e8c2fc2f3984bd96f
Raju <cmant47@outlook.com>
2021-01-08 18:51:36 +0000

    Added 2 memcached nodes and load balancing will be done on client side

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Run the below commands:
 docker-compose rm
+mvn package
 docker-compose up --build
 
 if you make changes run 'mvn package' and then run 'docker-compose up --build'
@@ -9,3 +10,4 @@ localhost:80/test
 http://localhost:9000/stats - haproxy stats, user name: admin, password: admin
 
 Run this command to start memcached outside docker composed: 'docker run --name sd-memcache --publish 11211:11211  -d memcached'
+use this link https://serverfault.com/a/674104 to see memcached server data. you need to download https://raw.githubusercontent.com/memcached/memcached/master/scripts/memcached-tool script and run it in local
--- src/main/java/com/sd/demo/config/MemcachedConfig.java
+++ src/main/java/com/sd/demo/config/MemcachedConfig.java
@@ -1,7 +1,10 @@
 package com.sd.demo.config;
 
 import net.rubyeye.xmemcached.MemcachedClient;
-import net.rubyeye.xmemcached.XMemcachedClient;
+import net.rubyeye.xmemcached.MemcachedClientBuilder;
+import net.rubyeye.xmemcached.XMemcachedClientBuilder;
+import net.rubyeye.xmemcached.impl.ElectionMemcachedSessionLocator;
+import net.rubyeye.xmemcached.utils.AddrUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +19,9 @@ public class MemcachedConfig {
 
     @Bean
     public MemcachedClient memcachedClient() throws IOException {
-        return new XMemcachedClient(host, 11211);
+        MemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil.getAddresses(host));
+        // use consistent hashing for client distribution strategy
+        builder.setSessionLocator(new ElectionMemcachedSessionLocator());
+        return builder.build();
     }
 }
